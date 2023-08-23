953a4ed3bf09a35ec79353c37974d2a66f6001e4
Pranjali-0928 <74135806+Pranjali-0928@users.noreply.github.com>
2023-08-23 20:25:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+<div class="product">
+  <!-- ... Product details ... -->
+  <p>Price: $20</p>
+  <button class="btn btn-primary add-to-cart" data-price="20">Add to Cart</button>
+</div>
+
